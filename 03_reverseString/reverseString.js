8e41a1d28d13b30e5d7ea6ec604c8edb2f64bd7b
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-07-28 18:05:47 +0700

    finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = ''
+    for (let i=string.length; i>0; i--){
+        if (string ===''){
+            result = '';
+        } else {
+        result += string[i-1];
+        }
+    }
+    return result;
 };
-
+console.log(reverseString('hello'));
 // Do not edit below this line
 module.exports = reverseString;
