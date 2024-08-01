91d7a79c2144250616b8381f1b2e77694a69fd8d
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-08-01 18:54:38 +0700

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || 
+        end < 0 || 
+        !Number.isInteger(start) || 
+        !Number.isInteger(end) ||
+        Number.isNaN(start) ||
+        Number.isNaN(end)         
+        ){
+        return 'ERROR'
+    }
+    let result = 0;
+    // if (start > end) {
+    //     let temp = start;
+    //     start = end;
+    //     end = temp;
+    // }
+    
+    // Faster way
+    if (start > end) {
+        [start, end] = [end, start];
+    }
 
+    for (let i = start; i <= end; i++){
+        result += i;
+    }
+    return result
 };
 
 // Do not edit below this line
