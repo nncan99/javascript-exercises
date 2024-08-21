0a494e6c7bccc582be7abd85e6c803afb4f7c0cd
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-08-21 12:00:30 +0700

    Finish Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    //return a string that doesn't contain any space, punctuation
+    const formatedString = inputString.split(/[\s,.!]/).join("").toLowerCase();
+    
+    const secondHalfString = 
+        formatedString.length % 2 === 0?
+        formatedString.slice(formatedString.length/2):
+        formatedString.slice(formatedString.length/2 + 1);
+    
+    const firstHalfString = formatedString.slice(0, formatedString.length/2);
+    return firstHalfString === secondHalfString.split('').reverse().join('');
 };
 
 // Do not edit below this line
