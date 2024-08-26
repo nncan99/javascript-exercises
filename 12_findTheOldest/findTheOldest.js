a6be3825b58f31cf5340a1329fb6a8c68e46c856
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-08-26 19:21:37 +0700

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayOfPeople) {
+    let now = new Date();
+    
+    const maxAge = Math.max(...arrayOfPeople.map(person => {
+        return  person.yearOfDeath ?
+                person.yearOfDeath - person.yearOfBirth :
+                now.getFullYear() - person.yearOfBirth;
+    }))
 
+    return arrayOfPeople.find( person => {
+        let age = person.yearOfDeath ?
+                  person.yearOfDeath - person.yearOfBirth :
+                  now.getFullYear() - person.yearOfBirth;
+        return age === maxAge
+    })
+    
 };
 
 // Do not edit below this line
